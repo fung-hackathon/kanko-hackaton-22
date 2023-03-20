a95c0387d7184b7e9f07887a2bea5c6a0e22c492
Teruki Tada <tadateruki.public@gmail.com>
2023-03-20 16:41:24 +0900

    [add] テキストとコマンドの識別

---
+++ app/domain/bot/command/command.go
@@ -0,0 +1,9 @@
+package command
+
+func ReadCommand(text string) (string, error) {
+	return "これはコマンドです", nil
+}
+
+func ReadText(text string) (string, error) {
+	return "これはテキストです", nil
+}
---
+++ app/infra/firestore.go
@@ -0,0 +1,50 @@
+package infra
+
+import (
+	"context"
+	"errors"
+	"kanko-hackaton-22/app/config"
+	"log"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
+)
+
+var (
+	ErrFirebaseInit = errors.New("failed to initialize firebase instance")
+	ErrFirestore    = errors.New("failed to establish connection to Firestore")
+)
+
+type Firestore struct {
+	Client  *firestore.Client
+	Context context.Context
+}
+
+func Initialize() (*Firestore, error) {
+	ctx := context.Background()
+	opt := option.WithCredentialsFile(config.GOOGLE_APPLICATION_CREDENTIALS)
+	//conf := &firebase.Config{ProjectID: "funhackathon22"}
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		return nil, ErrFirebaseInit
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Println(err)
+		return nil, ErrFirestore
+	}
+
+	return &Firestore{Client: client, Context: ctx}, nil
+}
+
+var FIRESTORE *Firestore
+
+func init() {
+	var err error
+	FIRESTORE, err = Initialize()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
